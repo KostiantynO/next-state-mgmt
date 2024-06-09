c6b3e50b5b740051e3bd09230a4120698d6084d5
Kostiantyn Ochenash <Ochenashk@gmail.com>
2024-06-09 08:09:45 +0300

    adds init cart from server and handles it to client

--- 01-starting-point/src/app/components/CartContext.tsx
+++ 01-starting-point/src/app/components/CartContext.tsx
@@ -5,15 +5,20 @@ import { createContext, useContext, useState } from 'react';
 import type { Cart } from '@/api/types';
 import type { Dispatch, ReactNode, SetStateAction } from 'react';
 
-const initCart = { products: [] } satisfies Cart;
-const useCartState = () => useState<Cart>(initCart);
+const useCartState = (initialCart: Cart) => useState<Cart>(initialCart);
 
 type CartState = [Cart, Dispatch<SetStateAction<Cart>>];
 
 const CartContext = createContext<CartState | null>(null);
 
-export const CartProvider = ({ children }: { children: ReactNode }) => {
-  const cartTuple = useCartState();
+export const CartProvider = ({
+  initialCart,
+  children,
+}: {
+  initialCart: Cart;
+  children: ReactNode;
+}) => {
+  const cartTuple = useCartState(initialCart);
 
   return (
     <CartContext.Provider value={cartTuple}>{children}</CartContext.Provider>
--- 01-starting-point/src/app/layout.tsx
+++ 01-starting-point/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import { clearCart } from '@/api/cart';
+import { clearCart, getCart } from '@/api/cart';
 
 import { CartProvider } from './components/CartContext';
 import { Header } from './components/Header';
@@ -23,6 +23,8 @@ export default async function RootLayout({
 }: {
   children: ReactNode;
 }) {
+  const cart = await getCart();
+
   const clearCartAction = async () => {
     'use server';
     return await clearCart();
@@ -31,7 +33,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CartProvider>
+        <CartProvider initialCart={cart}>
           <Header clearCartAction={clearCartAction} />
           <main className="mx-auto max-w-3xl">{children}</main>
         </CartProvider>
